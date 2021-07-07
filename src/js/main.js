17717898da5b7d5cf82b51dc66f11ba017297ecd
Дима Мироненко <city7gor@gmail.com>
2021-07-07 12:29:03 +0300

    Fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,3 @@
-// Hide Product item
-const productCart = document.querySelectorAll('.products__item')
-const productBtn = document.querySelector('.products__btn')
-
-for (let i = 0; i < productCart.length; i++) {
-  if (i > 7) {
-    productCart[i].style.display = 'none'
-  }
-}
-
-productBtn.addEventListener('click', i => {
-  productCart.forEach(item => {
-    item.style.display = 'block'
-  })
-  productBtn.style.display = 'none'
-})
-
 // Subscribe Form
 const subscribeForm = document.querySelector('.footer__subscribe-form')
 const subscribeBtn = document.querySelector('.footer__subscribe-form button')
@@ -44,7 +27,7 @@ addToCartLinks.forEach(item => {
 const header = document.querySelector('header')
 
 window.addEventListener('scroll', () => {
-  if(window.pageYOffset > 10) {
+  if (window.pageYOffset > 10) {
     header.classList.add('header--white')
   } else {
     header.classList.remove('header--white')
@@ -73,4 +56,41 @@ $('.tips__list').slick({
       },
     },
   ],
+})
+
+// Load products
+function loadProduct() {
+  fetch('https://fakestoreapi.com/products?limit=8')
+    .then(res => res.json())
+    .then(json => {
+      json.forEach(item => {
+        const product = document.createElement('div')
+        product.classList.add('products__item')
+        product.innerHTML = `
+        <div class="products__item-img">
+          <img src="${item.image}" alt="Syltherine">
+        </div>
+        <h5 class="title-h5 products__item-title">${item.title}</h5>
+        <p class="body-normal products__item-description">${item.description}</p>
+        <p class="body-xlarge products__item-price">Rp ${item.price}</p>
+        <div class="products__item-wrap"><a class="btn-main btn-main--white products__item-btn" href="#" data-text="Вы добавили товар${item.title}">Add to cart</a><a class="button-normal products__item-link" href="#" data-text="Поделиться товаром"><img src="img/share-white-icon.svg" alt=""><span>Share</span></a><a class="button-normal products__item-link" href="#" data-text="Вы поставили лайк"><img src="img/heart-white-icon.svg" alt=""><span>Like</span></a></div>
+      `
+        console.log(item)
+        document.querySelector('.products__list').appendChild(product)
+      })
+    })
+}
+
+loadProduct()
+
+const productBtn = document.querySelector('.products__btn')
+productBtn.addEventListener('click', () => {
+  loadProduct()
+})
+
+
+// Burger
+document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.header__burger').classList.toggle('active')
+  document.querySelector('.header__list').classList.toggle('active')
 })
